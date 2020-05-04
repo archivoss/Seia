e383f937b4aa07c2b2308a0481abc53e01db8e36
Gamaliel Troncoso <gamaliel.troncoso@gmail.com>
2020-05-03 22:47:34 -0400

    Se agrego background con un menú de selección vacio

--- src/seia/Seia.java
+++ src/seia/Seia.java
@@ -5,17 +5,32 @@
  */
 package seia;
 
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Gama
  */
-public class Seia {
+public class Seia extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Background.fxml"));       
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
