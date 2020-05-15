e29bbb159ed061dd853b1fe9e95c679880eb1d13
Francisco <quevedof98@gmail.com>
2020-05-14 22:14:55 -0400

    ultimo

--- src/seia/BackgroundController.java
+++ src/seia/BackgroundController.java
@@ -1,89 +1,10 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package seia;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-<<<<<<< HEAD
-import javafx.fxml.Initializable;
-=======
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-import javax.swing.*;
-import java.io.File;
-import java.io.IOException;
-import javafx.event.ActionEvent;
-import javafx.scene.layout.AnchorPane;
->>>>>>> Gamaliel_T
-
-/**
- * FXML Controller class
- *
- * @author Gama
- */
-public class BackgroundController implements Initializable {
-
-
-    /**
-     * Initializes the controller class.
-     */
-
-    
-    File archivoSeleccionado;
-    JFileChooser seleccionarArchivo;
-
-    @FXML
-    private Button addFileButton;
-    
-    @FXML
-    private AnchorPane anchorPane;
-    
-    @FXML
-    private void addFileButtonAction(ActionEvent event) throws IOException {
-        seleccionarArchivo = new JFileChooser();
-        seleccionarArchivo.showOpenDialog(null);
-        archivoSeleccionado = seleccionarArchivo.getSelectedFile();
-        archivoSeleccionado.setExecutable(true);
-    }
-    
-    @FXML
-    private void enter(MouseEvent event){
-        addFileButton.setStyle("-fx-background-color: #CCCCCC;");
-    }
-    
-    @FXML
-    private void release(MouseEvent event){
-        addFileButton.setStyle("-fx-background-color: #FFFFFF;");
-    }
-    
-    @FXML
-    private void exit(MouseEvent event){
-        addFileButton.setStyle("-fx-background-color: #FFFFFF;");
-    }
-    
-    @FXML
-    private void press(MouseEvent event){
-        addFileButton.setStyle("-fx-background-color: #BBBBBB;");
-    }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-
-    }    
-    
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package seia;
 
 import java.awt.BasicStroke;
@@ -187,4 +108,3 @@ public class BackgroundController implements Initializable {
     }    
     
 }
->>>>>>> Francisco_Q
